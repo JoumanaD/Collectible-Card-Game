28df9e3e5b217aebb8e3fb551a3d35a1d8c08455
Joumana <joumana.eldakar@gmail.com>
2023-11-05 21:58:30 +0100

    user

--- frontend/src/components/UserPage.jsx
+++ frontend/src/components/UserPage.jsx
@@ -35,7 +35,7 @@ function UserPage(){
                 <Link to={`/userCards/${user}`} state={{ userId: user }}></Link>
                 <Card.Img variant="top" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/41d3ac77-2823-4e18-8910-a48de63acf87/d8xl0ja-ca02368b-643f-42f4-9721-f3895e14690d.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzQxZDNhYzc3LTI4MjMtNGUxOC04OTEwLWE0OGRlNjNhY2Y4N1wvZDh4bDBqYS1jYTAyMzY4Yi02NDNmLTQyZjQtOTcyMS1mMzg5NWUxNDY5MGQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.dUhAjPqt1Rkudg3gHjqPNOQAuaA2wmXZ7T-Lq6L28O8" /> 
                 <Card.Body>
-                  <Card.Title>{index}</Card.Title>
+                  <Card.Title>User #{index}</Card.Title>
                 </Card.Body>
                 <Card.Footer>
                     <small className="text-muted">{user}</small>
